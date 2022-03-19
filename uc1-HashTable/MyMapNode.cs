febbfa38862787d54fbac99402308c5b6088b787
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-19 19:43:36 +0530

    added uc1

---
+++ uc1-HashTable/MyMapNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_practice_problems
+{
+    internal class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int postion)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[postion];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[postion] = linkedlist;
+            }
+            return linkedlist;
+        }
+    }
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+
+}
---
+++ uc1-HashTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_practice_problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HashTable Program!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "Not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index Value is " + hash5);
+
+            //Remove("2");
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2th index Value is " + hash2);
+        }
+    }
+
+}
+    
+
