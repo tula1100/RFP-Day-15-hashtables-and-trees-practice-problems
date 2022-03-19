cc802d23c934d2ccbe93ce9180eeab1481a6e832
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-19 19:45:49 +0530

    added uc2

---
+++ uc2-HashTables/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day15_uc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HashTable Program!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(18);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "beacuse");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "beacuse");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+
+            string hash5 = hash.Get("10");
+            Console.WriteLine("5th index Value is : " + hash5);
+
+
+            string hash2 = hash.Get("18");
+            Console.WriteLine("2th index Value is : " + hash2);
+        }
+    }
+}
+        }
+    }
+}
