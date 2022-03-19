b7b43ea4995742c86f88229ad2e906cc29e4555c
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-19 19:50:36 +0530

    added uc1

---
+++ uc1-binarytree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day15PPBinarySearchTree
+{
+    internal class BinarySearchTree
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program!");
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(3);
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.Display();
+        }
+    }
+}
